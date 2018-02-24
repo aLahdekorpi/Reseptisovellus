19c2916bc6d83965a532966c6ace9259e7f4c42f
Minä <j.halone97@gmail.com>
2018-02-24 21:19:16 +0200

    Lisäsin sparkit Mainiin ja päivitin pom.xml tarvittavia riippuvuuksia.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,99 @@
-
 package main;
 
+import dao.AnnosDao;
+import dao.AnnosRaakaAineDao;
+import dao.RaakaAineDao;
+import database.Database;
+import domain.Annos;
+import domain.AnnosRaakaAine;
+import domain.RaakaAine;
+import java.sql.SQLException;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 public class Main {
-    
+
+    public static void main(String[] args) throws Exception {
+        // ottaa yhteyden herokun tarjoamaan portiin
+        //  if (System.getenv("PORT") != null) {
+        //    Spark.port(Integer.valueOf(System.getenv("PORT")));
+        //}
+        //Database pitää muokata Herokuun sopivaksi
+        Database database = new Database("jdbc:sqlite:reseptit.db");
+        AnnosDao annokset = new AnnosDao(database);
+        RaakaAineDao raaka_aine = new RaakaAineDao(database);
+        AnnosRaakaAineDao annosraakaaine = new AnnosRaakaAineDao(database);
+
+        Spark.get("*", (req, res) -> {
+            HashMap map = new HashMap();
+            map.put("annokset", annokset.findAll());
+
+            return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
+
+        Spark.get("/raaka-aineet", (req, res) -> {
+            HashMap map = new HashMap();
+            map.put("raaka_aineeet", raaka_aine.findAll());
+
+            return new ModelAndView(map, "raaka-aineet");
+        }, new ThymeleafTemplateEngine());
+
+        Spark.post("/raaka-aineet", (req, res) -> {
+            RaakaAine raaka
+                    = new RaakaAine(-1, req.queryParams("nimi"));
+            raaka_aine.saveOrUpdate(raaka);
+
+            res.redirect("/raaka-aineet");
+            return "";
+        });
+
+        Spark.get("/raaka-aineet/:id", (req, res) -> {
+            HashMap map = new HashMap();
+            Integer id = Integer.parseInt(req.params(":id"));
+            map.put("raaka_aine", raaka_aine.findOne(id));
+
+            //tarvitsee raakaaineDaoon metodin jotta voidaan listata raaka-aineet
+            return new ModelAndView(map, "raaka-aine");
+        }, new ThymeleafTemplateEngine());
+
+        Spark.get("/annokset", (req, res) -> {
+            HashMap map = new HashMap();
+            map.put("annokset", annokset.findAll());
+            return new ModelAndView(map, "annokset");
+        }, new ThymeleafTemplateEngine());
+
+        // Annosten luominen ja raaka-aineiden yhdistäminen
+        Spark.post("/annokset", (req, res) -> {
+            //uusi annos
+            String annosnimi = req.queryParams("nimi");
+            Annos a = new Annos(-1, annosnimi);
+            annokset.saveOrUpdate(a);
+            //yhdista raaka_aineet
+            int annos_id = Integer.valueOf(req.queryParams("annokseen"));
+            int raakaaine_id = Integer.valueOf(req.queryParams("raaka-aine"));
+            String jarjestys = req.queryParams("jarjestys");
+            String maara = req.queryParams("maara");
+            String ohje = req.queryParams("ohje");
+            AnnosRaakaAine ara
+                    = new AnnosRaakaAine(-1, annos_id, raakaaine_id, jarjestys, maara, ohje);
+            annosraakaaine.saveOrUpdate(ara);
+            
+            res.redirect("/annokset");
+            return "";
+        });
+        
+         Spark.get("/annokset/:id", (req, res) -> {
+            HashMap map = new HashMap();
+            Integer id = Integer.parseInt(req.params(":id"));
+            map.put("annos", raaka_aine.findOne(id));
+            //tarvitsee raakaaineDaoon metodin jotta voidaan listata raaka-aineet
+            return new ModelAndView(map, "annos");
+        }, new ThymeleafTemplateEngine());
+        
+        
+
+    }
+
 }
