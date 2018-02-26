d3445ae5bfc05e9a4e21eb3b7214be72da15d8a8
Alex Lähdekorpi <lalex@dhcp-asv-70.helsinki.fi>
2018-02-26 15:54:49 +0200

    eka html testi

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -60,6 +60,7 @@ public class Main {
         Spark.get("/annokset", (req, res) -> {
             HashMap map = new HashMap();
             map.put("annokset", annokset.findAll());
+            map.put("raaka-aineet", raaka_aine.findAll());
             return new ModelAndView(map, "annokset");
         }, new ThymeleafTemplateEngine());
 
