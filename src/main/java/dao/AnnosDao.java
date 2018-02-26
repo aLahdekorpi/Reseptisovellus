9aab1fb3a9e9ad833e14cc369e20f902d5c82178
Alex Lähdekorpi <lalex@dhcp-asv-70.helsinki.fi>
2018-02-26 15:21:10 +0200

    eka html testi

--- src/main/java/dao/AnnosDao.java
+++ src/main/java/dao/AnnosDao.java
@@ -1,46 +1,46 @@
-
 package dao;
-
+ 
 import database.Database;
 import domain.Annos;
+import domain.RaakaAine;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 public class AnnosDao implements Dao<Annos, Integer> {
-
+ 
     private Database database;
-
+ 
     public AnnosDao(Database database) {
         this.database = database;
     }
-
+ 
     public Annos findOne(Integer key) throws SQLException, Exception {
         Connection connection = database.getConnection();
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Annos WHERE id = ?");
         stmt.setObject(1, key);
-
+ 
         ResultSet rs = stmt.executeQuery();
         boolean hasOne = rs.next();
         if (!hasOne) {
             return null;
         }
-
+ 
         Integer id = rs.getInt("id");
         String nimi = rs.getString("nimi");
-
+ 
         Annos annos = new Annos(id, nimi);
-
+ 
         rs.close();
         stmt.close();
         connection.close();
-
+ 
         return annos;
     }
-
+ 
     public Annos saveOrUpdate(Annos annos) throws SQLException, Exception {
         if (annos.getId() == null) {
             return save(annos);
@@ -48,84 +48,108 @@ public class AnnosDao implements Dao<Annos, Integer> {
             return update(annos);
         }
     }
-
+ 
     private Annos save(Annos annos) throws SQLException, Exception {
-
+ 
         Connection conn = database.getConnection();
         PreparedStatement stmt = conn.prepareStatement("INSERT INTO Annos"
                 + " (id, nimi)"
                 + " VALUES (?, ?)");
         stmt.setInt(1, annos.getId());
         stmt.setString(2, annos.getNimi());
-
+ 
         stmt.executeUpdate();
         stmt.close();
-
+ 
         stmt = conn.prepareStatement("SELECT * FROM Annos"
                 + " WHERE id = ? AND nimi = ?");
         stmt.setInt(1, annos.getId());
         stmt.setString(2, annos.getNimi());
-
+ 
         ResultSet rs = stmt.executeQuery();
         rs.next();
-
+ 
         Annos a = new Annos(rs.getInt("id"), rs.getString("nimi"));
-
+ 
         stmt.close();
         rs.close();
-
+ 
         conn.close();
-
+ 
         return a;
     }
-
+ 
     private Annos update(Annos annos) throws SQLException, Exception {
-
+ 
         Connection conn = database.getConnection();
         PreparedStatement stmt = conn.prepareStatement("UPDATE Annos SET"
                 + "id = ?, nimi = ? WHERE id = ?");
         stmt.setInt(1, annos.getId());
         stmt.setString(2, annos.getNimi());
         stmt.setInt(3, annos.getId());
-
+ 
         stmt.executeUpdate();
-
+ 
         stmt.close();
         conn.close();
-
+ 
         return annos;
     }
-
+ 
     @Override
     public List<Annos> findAll() throws SQLException, Exception {
-
+ 
         Connection connection = database.getConnection();
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Annos");
-
+ 
         ResultSet rs = stmt.executeQuery();
         List<Annos> annokset = new ArrayList<>();
         while (rs.next()) {
             Integer id = rs.getInt("id");
             String nimi = rs.getString("nimi");
-
+ 
             annokset.add(new Annos(id, nimi));
         }
-
+ 
         rs.close();
         stmt.close();
         connection.close();
-
+ 
         return annokset;
     }
-
+   
+     public List<RaakaAine> findRaakaAineet(Integer key) throws SQLException, Exception {
+     
+         Connection connection = database.getConnection();
+         PreparedStatement stmt = connection.prepareStatement("SELECT raakaine FROM raakaaine  INNER JOIN annosraakaaine ON annosraakaaine.raaka_aine_id = raakaaine.id WHERE annosraakaaine.annos_id = ? ");
+         stmt.setInt(1, key);
+         
+         ResultSet rs = stmt.executeQuery();
+         List<RaakaAine> raakaaineet = new ArrayList<>();
+         while(rs.next()) {
+             Integer id = rs.getInt("id");
+             String nimi = rs.getString("nimi");
+             
+             raakaaineet.add(new RaakaAine(id, nimi));
+         }
+         
+         rs.close();
+         stmt.close();
+         connection.close();
+         
+         return raakaaineet;
+         
+         
+     }
+ 
     @Override
     public void delete(Integer key) throws SQLException, Exception {
         Connection conn = database.getConnection();
         PreparedStatement stmt = conn.prepareStatement("DELETE FROM Annos WHERE id = ?");
         stmt.setInt(1, key);
-
+ 
         stmt.executeUpdate();
-
+ 
         stmt.close();
         conn.close();
     }
--- src/main/java/dao/RaakaAineDao.java
+++ src/main/java/dao/RaakaAineDao.java
@@ -1,7 +1,7 @@
-
 package dao;
-
+ 
 import database.Database;
+import domain.Annos;
 import domain.RaakaAine;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,38 +9,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 public class RaakaAineDao implements Dao<RaakaAine, Integer> {
-
+ 
     private Database database;
-
+ 
     public RaakaAineDao(Database database) {
         this.database = database;
     }
-
+ 
     public RaakaAine findOne(Integer key) throws SQLException, Exception {
         Connection connection = database.getConnection();
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM RaakaAine WHERE id = ?");
         stmt.setObject(1, key);
-
+ 
         ResultSet rs = stmt.executeQuery();
         boolean hasOne = rs.next();
         if (!hasOne) {
             return null;
         }
-
+ 
         Integer id = rs.getInt("id");
         String nimi = rs.getString("nimi");
-
+ 
         RaakaAine raakaaine = new RaakaAine(id, nimi);
-
+ 
         rs.close();
         stmt.close();
         connection.close();
-
+ 
         return raakaaine;
     }
-
+ 
     public RaakaAine saveOrUpdate(RaakaAine raakaaine) throws SQLException, Exception {
         if (raakaaine.getId() == null) {
             return save(raakaaine);
@@ -48,84 +48,108 @@ public class RaakaAineDao implements Dao<RaakaAine, Integer> {
             return update(raakaaine);
         }
     }
-
+ 
     private RaakaAine save(RaakaAine raakaaine) throws SQLException, Exception {
-
+ 
         Connection conn = database.getConnection();
         PreparedStatement stmt = conn.prepareStatement("INSERT INTO RaakaAine"
                 + " (id, nimi)"
                 + " VALUES (?, ?)");
         stmt.setInt(1, raakaaine.getId());
         stmt.setString(2, raakaaine.getNimi());
-
+ 
         stmt.executeUpdate();
         stmt.close();
-
+ 
         stmt = conn.prepareStatement("SELECT * FROM RaakaAine"
                 + " WHERE id = ? AND nimi = ?");
         stmt.setInt(1, raakaaine.getId());
         stmt.setString(2, raakaaine.getNimi());
-
+ 
         ResultSet rs = stmt.executeQuery();
         rs.next();
-
+ 
         RaakaAine a = new RaakaAine(rs.getInt("id"), rs.getString("nimi"));
-
+ 
         stmt.close();
         rs.close();
-
+ 
         conn.close();
-
+ 
         return a;
     }
-
+ 
     private RaakaAine update(RaakaAine raakaaine) throws SQLException, Exception {
-
+ 
         Connection conn = database.getConnection();
         PreparedStatement stmt = conn.prepareStatement("UPDATE RaakaAine SET"
                 + "id = ?, nimi = ? WHERE id = ?");
         stmt.setInt(1, raakaaine.getId());
         stmt.setString(2, raakaaine.getNimi());
         stmt.setInt(3, raakaaine.getId());
-
+ 
         stmt.executeUpdate();
-
+ 
         stmt.close();
         conn.close();
-
+ 
         return raakaaine;
     }
-
+ 
     @Override
     public List<RaakaAine> findAll() throws SQLException, Exception {
-
+ 
         Connection connection = database.getConnection();
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM RaakaAine");
-
+ 
         ResultSet rs = stmt.executeQuery();
         List<RaakaAine> raakaaineet = new ArrayList<>();
         while (rs.next()) {
             Integer id = rs.getInt("id");
             String nimi = rs.getString("nimi");
-
+ 
             raakaaineet.add(new RaakaAine(id, nimi));
         }
-
+ 
         rs.close();
         stmt.close();
         connection.close();
-
+ 
         return raakaaineet;
     }
-
+ 
+    public int countAnnokset(Integer key) throws SQLException, Exception {
+ 
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT COUNT(*) FROM annos INNER JOIN annosraakaaine ON annosraakaaine.annos_id = annos.id WHERE annosraakaaine.raaka_aine_id = ? ");
+        stmt.setInt(1, key);
+ 
+        ResultSet rs = stmt.executeQuery();
+        int count;
+ 
+        if (rs.next()) {
+            count = rs.getInt(1);
+ 
+        } else {
+            count = 0;
+        }
+ 
+        rs.close();
+        stmt.close();
+        connection.close();
+ 
+        return count;
+ 
+    }
+ 
     @Override
     public void delete(Integer key) throws SQLException, Exception {
         Connection conn = database.getConnection();
         PreparedStatement stmt = conn.prepareStatement("DELETE FROM RaakaAine WHERE id = ?");
         stmt.setInt(1, key);
-
+ 
         stmt.executeUpdate();
-
+ 
         stmt.close();
         conn.close();
     }
