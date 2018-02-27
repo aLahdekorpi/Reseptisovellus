01b388c1c21097d1c9c649a400f776b63c035400
Alex Lähdekorpi <lalex@dhcp-asv-70.helsinki.fi>
2018-02-27 12:14:14 +0200

    jea

--- src/main/java/domain/AnnosRaakaAine.java
+++ src/main/java/domain/AnnosRaakaAine.java
@@ -1,16 +1,16 @@
-
+ 
 package domain;
-
+ 
 public class AnnosRaakaAine {
-
+ 
     private Integer id;
     private Integer annosId;
     private Integer raakaAineId;
-    private String jarjestys;
+    private Integer jarjestys;
     private String maara;
     private String ohje;
-
-    public AnnosRaakaAine(Integer id, Integer annosId, Integer raakaAineId, String jarjestys, String maara, String ohje) {
+ 
+    public AnnosRaakaAine(Integer id, Integer annosId, Integer raakaAineId, int jarjestys, String maara, String ohje) {
         this.id = id;
         this.annosId = annosId;
         this.raakaAineId = raakaAineId;
@@ -18,27 +18,27 @@ public class AnnosRaakaAine {
         this.maara = maara;
         this.ohje = ohje;
     }
-
+ 
     public Integer getId() {
         return id;
     }
-
+ 
     public Integer getAnnosId() {
         return annosId;
     }
-
+ 
     public Integer getRaakaAineId() {
         return raakaAineId;
     }
-
-    public String getJarjestys() {
+ 
+    public int getJarjestys() {
         return jarjestys;
     }
-
+ 
     public String getMaara() {
         return maara;
     }
-
+ 
     public String getOhje() {
         return ohje;
     }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,5 @@
 package main;
-
+ 
 import dao.AnnosDao;
 import dao.AnnosRaakaAineDao;
 import dao.RaakaAineDao;
@@ -7,17 +7,15 @@ import database.Database;
 import domain.Annos;
 import domain.AnnosRaakaAine;
 import domain.RaakaAine;
-import java.sql.SQLException;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.Spark;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
-import org.thymeleaf.context.IWebContext;
-
+ 
 public class Main {
-
+ 
     public static void main(String[] args) throws Exception {
-          if (System.getenv("PORT") != null) {
+        if (System.getenv("PORT") != null) {
             Spark.port(Integer.valueOf(System.getenv("PORT")));
         }
         //Database pitää muokata Herokuun sopivaksi
@@ -25,47 +23,45 @@ public class Main {
         AnnosDao annokset = new AnnosDao(database);
         RaakaAineDao raaka_aine = new RaakaAineDao(database);
         AnnosRaakaAineDao annosraakaaine = new AnnosRaakaAineDao(database);
-
+ 
         Spark.get("/", (req, res) -> {
             HashMap map = new HashMap();
             map.put("annokset", annokset.findAll());
-
+ 
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-
+ 
         Spark.get("/raaka-aineet", (req, res) -> {
             HashMap map = new HashMap();
             map.put("raaka_aineet", raaka_aine.findAll());
-
+ 
             return new ModelAndView(map, "raaka-aineet");
         }, new ThymeleafTemplateEngine());
-
+ 
         Spark.post("/raaka-aineet", (req, res) -> {
             RaakaAine raaka
                     = new RaakaAine(-1, req.queryParams("nimi"));
             raaka_aine.saveOrUpdate(raaka);
-
+ 
             res.redirect("/raaka-aineet");
             return "";
         });
-
+ 
         Spark.get("/raaka-aineet/:id", (req, res) -> {
             HashMap map = new HashMap();
             Integer id = Integer.parseInt(req.params(":id"));
-            map.put("raakaAine", raaka_aine.findOne(id));
+            map.put("raaka_aine", raaka_aine.findOne(id));
             map.put("annoksessa", raaka_aine.countAnnokset(id));
-           
+ 
             return new ModelAndView(map, "raaka-aine");
         }, new ThymeleafTemplateEngine());
-
-
+ 
         Spark.get("/annokset", (req, res) -> {
             HashMap map = new HashMap();
             map.put("annokset", annokset.findAll());
-            map.put("raakaAineet", raaka_aine.findAll());
             return new ModelAndView(map, "annokset");
         }, new ThymeleafTemplateEngine());
-
+ 
         // Annosten luominen ja raaka-aineiden yhdistäminen
         Spark.post("/annokset", (req, res) -> {
             //uusi annos
@@ -75,27 +71,40 @@ public class Main {
             //yhdista raaka_aineet
             int annos_id = Integer.valueOf(req.queryParams("annokseen"));
             int raakaaine_id = Integer.valueOf(req.queryParams("raaka-aine"));
-            String jarjestys = req.queryParams("jarjestys");
+           
             String maara = req.queryParams("maara");
             String ohje = req.queryParams("ohje");
+           
+            int jarjestys;
+            try {
+                jarjestys = Integer.valueOf(req.queryParams("jarjestys"));
+            } catch (NumberFormatException e) {
+                res.status(418);
+                return "Väärä tai tyhjä syöte";
+            }
+ 
+            if (maara.equals("") || ohje.equals("")) {
+                res.status(418);
+                return "Väärä tai tyhjä syöte";
+ 
+            }
+ 
             AnnosRaakaAine ara
                     = new AnnosRaakaAine(-1, annos_id, raakaaine_id, jarjestys, maara, ohje);
             annosraakaaine.saveOrUpdate(ara);
-            
+ 
             res.redirect("/annokset");
             return "";
         });
-        
-          Spark.get("/annokset/:id", (req, res) -> {
+ 
+        Spark.get("/annokset/:id", (req, res) -> {
             HashMap map = new HashMap();
             Integer id = Integer.parseInt(req.params(":id"));
-            map.put("annos", annokset.findOne(id));
-            map.put("raakaAineet", annokset.getRaakaAineet(id));
+            map.put("annos", raaka_aine.findOne(id));
+            map.put("raaka-aineet", annokset.getRaakaAineet(id));
             return new ModelAndView(map, "annos");
         }, new ThymeleafTemplateEngine());
-        
-        
-
+ 
     }
-
+ 
 }
